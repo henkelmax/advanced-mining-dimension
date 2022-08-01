6c13a2c00b0cede4ea528eb36a86fc753aff0ae1
Max Henkel <max@maxhenkel.de>
2022-08-01 12:04:24 +0200

    Fix experimental warning config option

--- src/main/java/de/maxhenkel/miningdimension/mixin/MinecraftMixin.java
+++ src/main/java/de/maxhenkel/miningdimension/mixin/MinecraftMixin.java
@@ -1,24 +1,22 @@
 package de.maxhenkel.miningdimension.mixin;
 
+import com.mojang.serialization.Lifecycle;
 import de.maxhenkel.miningdimension.Main;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.storage.WorldData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Minecraft.class)
-public abstract class MinecraftMixin {
+public class MinecraftMixin {
 
-    /*@ModifyVariable(
-            method = "loadWorld(Ljava/lang/String;Lnet/minecraft/util/registry/DynamicRegistries$Impl;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/Minecraft$WorldSelectionType;Z)V",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft$WorldSelectionType;NONE:Lnet/minecraft/client/Minecraft$WorldSelectionType;", ordinal = 0),
-            name = "flag1"
-    )
-    private boolean setFlag(boolean flag) {
-        if (Main.CLIENT_CONFIG.showCustomWorldWarning.get()) {
-            return flag;
+    @Redirect(method = "doLoadLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/WorldData;worldGenSettingsLifecycle()Lcom/mojang/serialization/Lifecycle;"))
+    public Lifecycle isOldCustomizedWorld(WorldData data) {
+        if (!Main.CLIENT_CONFIG.showCustomWorldWarning.get()) {
+            return Lifecycle.stable();
         }
-        return false;
-    }*/
+        return data.worldGenSettingsLifecycle();
+    }
 
 }
