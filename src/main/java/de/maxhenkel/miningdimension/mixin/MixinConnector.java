db115ecd7fbafcdedbd92914913fe41dbcf11104
Max Henkel <max@maxhenkel.de>
2023-12-21 12:13:53 +0100

    Backport changes

--- src/main/java/de/maxhenkel/miningdimension/mixin/MixinConnector.java
+++ src/main/java/de/maxhenkel/miningdimension/mixin/MixinConnector.java
@@ -1,6 +1,5 @@
 package de.maxhenkel.miningdimension.mixin;
 
-import de.maxhenkel.miningdimension.Main;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.connect.IMixinConnector;
 
@@ -8,7 +7,7 @@ public class MixinConnector implements IMixinConnector {
 
     @Override
     public void connect() {
-        Mixins.addConfiguration("assets/" + Main.MODID + "/mining_dimension.mixins.json");
+        Mixins.addConfiguration("mining_dimension.mixins.json");
     }
 
 }
