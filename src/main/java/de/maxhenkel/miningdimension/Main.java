8cc8f99229da1bd08f77c99be4d1b1f2fc07948d
Max Henkel <max@maxhenkel.de>
2024-01-04 13:46:08 +0100

    Update NeoForge

--- src/main/java/de/maxhenkel/miningdimension/Main.java
+++ src/main/java/de/maxhenkel/miningdimension/Main.java
@@ -14,10 +14,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
@@ -43,16 +43,16 @@ public class Main {
     public static ServerConfig SERVER_CONFIG;
     public static ClientConfig CLIENT_CONFIG;
 
-    public Main() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(CreativeTabEvents::onCreativeModeTabBuildContents);
+    public Main(IEventBus eventBus) {
+        eventBus.addListener(this::commonSetup);
+        eventBus.addListener(CreativeTabEvents::onCreativeModeTabBuildContents);
 
         SERVER_CONFIG = CommonRegistry.registerConfig(ModConfig.Type.SERVER, ServerConfig.class, true);
         CLIENT_CONFIG = CommonRegistry.registerConfig(ModConfig.Type.CLIENT, ClientConfig.class);
 
-        BLOCK_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
-        ITEM_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
-        BLOCK_ENTITY_REGISTER.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCK_REGISTER.register(eventBus);
+        ITEM_REGISTER.register(eventBus);
+        BLOCK_ENTITY_REGISTER.register(eventBus);
     }
 
     public void commonSetup(FMLCommonSetupEvent event) {
